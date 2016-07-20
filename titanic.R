5d1c69f56c49e28430913894f80a31b4aed68cf9
dannielwinarto <dannielwinarto@gmail.com>
2016-07-20 01:07:45 -0700

    still 0.78947 public score

--- titanic.R
+++ titanic.R
@@ -37,24 +37,27 @@
 # travelled with very close friends or neighbors in a village, however,
 # the definitions do not support such relations.
 
-
 rm(list = ls())
 setwd("C:/Users/dan_9/Desktop/COURSERA + SELF STUDY/Kaggle/Titanic")
 
 library(dplyr)
 library(ggplot2)
+library(gridExtra)
 library(randomForest)
-library(stringr)
+library(stringr) 
+library(missForest) # imputation
+library(mice) # imputation
+library(VIM) # visualization missing value
 
 genderclassmodel = read.csv("genderclassmodel.csv")
 gendermodel = read.csv("gendermodel.csv")
 training = read.csv("train.csv")
 testing = read.csv("test.csv")
-head(testing)
 
 glimpse(training)
 head(training)
 tail(training)
+glimpse(testing)
 head(testing)
 tail(testing)
 
@@ -66,40 +69,25 @@ testing = testing[,c(1, grep("Survived", colnames(testing)), 2 :  (grep("Survive
 full = rbind(training, testing)
 glimpse(full)
 
+
+######################################################################
+# PART 1 - DATA CLEANING, MANIPULATION, AND EXPLORATORY ANALYSIS
+######################################################################
+
 # column name should be character instead of factor 
 full$Name = as.character(full$Name)
 
-# deleting the columns: ticket, cabin
-# training$Ticket = NULL
-# training$Cabin = NULL
+# checking how many NA for each column for both training and testing data
+trainingNAs = aggr(training, numbers=T, sortVars=F)
+trainingNAs
+testingNAs = aggr(testing, numbers=T, sortVars=F)
+testingNAs
+fullNAs = aggr(full, numbers=T, sortVars=F)
+fullNAs
 
-# checking how many NA for each column
-colnum = NULL
-for(i in 1 : ncol(full)){
-  colnum[i] = sum(is.na(full[,i]))
-}
-
-
-colNull  = data.frame(NULL)
-for (i in 1:length(names(full))) {
-  colNull[i,1] = names(full)[i]
-  colNull[i,2] = sum(is.na(full[,i]))
-}
-
-# V1  V2
-# 1  PassengerId   0
-# 2     Survived 418
-# 3       Pclass   0
-# 4         Name   0
-# 5          Sex   0
-# 6          Age 263
-# 7        SibSp   0
-# 8        Parch   0
-# 9       Ticket   0
-# 10        Fare   1
-# 11       Cabin   0
-# 12    Embarked   0
-# 13   honorific   0
+# We need to see Fare column and fill it with the value that make sense,
+# in this case, we will fill the cell with based on the passanger's other attributes, compared with other passanger,
+# which is Pclass, Age >=50, Embarked from Southampton
 
 which(is.na(full$Fare))
 # [1] 1044
@@ -114,40 +102,42 @@ full %>% filter(!is.na(Fare), Pclass == 3, Age >= 50, Embarked == "S" ) %>% summ
 
 full$Fare[1044] = 8.43042
 
+# Since column Age contains the most of NAs, 177 NAs, we better to do exploratory analysis with these raw data, before we perform
+# imputation of missing values. Because our imputation may alter our view towards raw dataset if we perform the exploratory later
 
-# investigating the age distribution
+# In these exploratory analysis, we will exclude the observations with NAs in Age
 
-table(full$Age)
-
-ggplot(full[!is.na(full$Age),], aes(x = Age )) +
-  geom_histogram() + 
+plotAgeRaw1 = ggplot(full[!is.na(full$Age),], aes(x = Age )) +
+  geom_histogram() +
   facet_grid(~Sex) +
-  scale_y_continuous(breaks = seq(0,80,5)) +
+  coord_cartesian(ylim =  seq(0,100,5))+
+  scale_y_continuous(breaks = seq(0,100,5)) +
   scale_x_continuous(breaks = seq(0,90,10)) +
-  ggtitle("Age Dsitribution of Passenger")
+  ggtitle("Age Dsitribution of Passenger by Gender\n RAWDATA")
 
-ggplot(full[!is.na(full$Age),], aes(x = Age, fill = as.factor(Sex))) +
+plotAgeRaw2 = ggplot(full[!is.na(full$Age),], aes(x = Age, fill = as.factor(Sex))) +
   geom_histogram(position = "identity", alpha = .4) +
   scale_fill_manual(values=c("green", "purple")) +
-  scale_y_continuous(breaks = seq(0,80,5))+
-  scale_x_continuous(breaks = seq(0,90,5)) +
-  ggtitle("Age Dsitribution of Passenger") +
+  coord_cartesian(ylim =  seq(0,100,5))+
+  scale_y_continuous(breaks = seq(0,100,5)) +
+  scale_x_continuous(breaks = seq(0,90,10)) +
+  ggtitle("Age Dsitribution of Passenger by Gender\n RAWDATA") +
   labs(fill="")
-
 
 medians = aggregate(Age ~  Sex, full, median)
 
-ggplot(full[!is.na(full$Age),], aes(x = Sex, y = Age)) +
+plotAgeRaw3 = ggplot(full[!is.na(full$Age),], aes(x = Sex, y = Age)) +
   geom_boxplot() +
   scale_y_continuous(breaks = seq(0,80,5)) +
-  geom_text( data = medians, aes( label = Age, y = Age), vjust = -.5)
-  
-full$Age[full$Sex == "female" & is.na(full$Age)] = 27
-full$Age[full$Sex == "male" & is.na(full$Age)] = 28
+  geom_text( data = medians, aes( label = Age, y = Age), vjust = -.5) +
+  ggtitle("Age Dsitribution of Passenger by Gender\n RAW")
 
-  
-# column age contains a lot of missing values (263 records), so we would like to analyze based on honorifics system on their name, 
-# and ultimately group it into adult male, adult female, young male, young female
+plotAgeRaw1
+plotAgeRaw2
+plotAgeRaw3
+
+# we would like to analyze based on honorifics system on their name, 
+# and ultimately group it into adult male, female, young male
 
 full$honorific = str_extract(full$Name, pattern = "\\,[ ][A-z]*[ ]*[A-z]*\\.")
 full$honorific = gsub(", ","",full$honorific)
@@ -158,10 +148,11 @@ table(full$honorific)
 
 SurvivalRateByHonorific = full %>%
   filter(Survived != "NA") %>%
-  group_by(honorific) %>% 
-  summarise(minAge = min(Age, na.rm = T),maxAge = max(Age, na.rm = T), PassengerCount = n(), 
+  group_by(honorific) %>%
+  summarise(minAge = min(Age, na.rm = T),maxAge = max(Age, na.rm = T), PassengerCount = n(),
             PassengerSurvived =  sum(Survived == 1), survivalRate = round(sum(Survived == 1)/n(),3)) %>%
   arrange(desc(PassengerSurvived))
+SurvivalRateByHonorific
 
 # We can clearly see there are 18 honorific name being used in this dataset, 
 # However we can classified all 18 into three different group:
@@ -172,7 +163,7 @@ SurvivalRateByHonorific = full %>%
 full$GenderAgeClass[full$honorific ==  "Miss." | full$honorific ==  "Mrs."| full$honorific ==  "Mlle."| 
                       full$honorific ==  "Lady."|full$honorific ==  "Mme."|full$honorific ==  "Ms."| 
                       full$honorific ==  "the Countess."|full$honorific ==  "Dona."] = "Female"
-                        
+
 full$GenderAgeClass[full$honorific ==  "Mr." |  full$honorific ==  "Dr."| full$honorific ==  "Col."|
                       full$honorific ==  "Major."| full$honorific ==  "Sir."|full$honorific ==  "Capt."|
                       full$honorific ==  "Don."| full$honorific ==  "Jonkheer."|full$honorific ==  "Rev."] = "Adult Male"
@@ -182,28 +173,27 @@ full$GenderAgeClass[full$honorific ==  "Master."] = "Young Male"
 
 SurvivalRateByGenderAge = full %>%
   filter(Survived != "NA") %>%
-  group_by(GenderAgeClass) %>% 
-  summarise(minAge = min(Age, na.rm = T),maxAge = max(Age, na.rm = T), PassengerCount = n(), 
+  group_by(GenderAgeClass) %>%
+  summarise(minAge = min(Age, na.rm = T),maxAge = max(Age, na.rm = T), PassengerCount = n(),
             PassengerSurvived =  sum(Survived == 1), survivalRate = round(sum(Survived == 1)/n(),3)) %>%
   arrange(desc(PassengerSurvived))
-  
-ggplot(SurvivalRateByGenderAge, aes(x = reorder(GenderAgeClass, -survivalRate), y = survivalRate, group = 1)) + 
+SurvivalRateByGenderAge
+
+ggplot(SurvivalRateByGenderAge, aes(x = reorder(GenderAgeClass, -survivalRate), y = survivalRate, group = 1)) +
   geom_line()+
   geom_text(aes(label = survivalRate), vjust = -.3, hjust = -.3 , size = 5, color = "red") +
   ggtitle("Survival Rate by Gender-Age Class")+
   ylim(0,1)+
-  xlab("Gender-Age Class") + 
+  xlab("Gender-Age Class") +
   ylab("Survival Rate")
 
 
-ggplot(full[!is.na(full$Survived),], aes(x = Survived, y = GenderAgeClass)) + 
+ggplot(full[!is.na(full$Survived),], aes(x = Survived, y = GenderAgeClass)) +
   ggtitle("Gender-Age Class survival cases")+
   scale_x_continuous( breaks = seq(0,1,1))+
-  xlab("Survived = 1, not-Survived = 0") + 
+  xlab("0 = not-Survived \t 1 = Survived ") +
   ylab("Gender Age Class")+
-  geom_jitter(width = .65, color = "blue")    
-
-# exploratory analysis
+  geom_jitter(width = .65, color = "blue")
 
 # Intuitively, it seems like there are strong correlation between variable "pclass" and "fare"
 # since pclass  = 1 means the highest and tend to have higher fare, then we expect strong negative correlation between there two variables
@@ -229,67 +219,69 @@ full$FareClass = FareClass
 levels(full$Embarked) = c("Unknown", "Cherbourg", "Queenstown", "Southampton")
 table(full$Embarked)
 
-ggplot(data = full, aes(x = Embarked, fill = FareClass)) + 
-  geom_bar() 
+ggplot(data = full, aes(x = Embarked, fill = FareClass)) +
+  geom_bar()
 
 SurvivalRateByFareClass = full %>%
   filter(Survived != "NA") %>%
-  group_by(FareClass) %>% 
-  summarise(PassengerCount = n(),PassengerSurvived =  sum(Survived == 1), 
+  group_by(FareClass) %>%
+  summarise(PassengerCount = n(),PassengerSurvived =  sum(Survived == 1),
             survivalRate = round(sum(Survived == 1)/n(),3)) %>%
   arrange(desc(FareClass))
+SurvivalRateByFareClass
 
-ggplot(SurvivalRateByFareClass, aes(x = FareClass, y = survivalRate, group = 1)) + 
+ggplot(SurvivalRateByFareClass, aes(x = FareClass, y = survivalRate, group = 1)) +
   geom_line(size = 1.2)+
   geom_text(aes(label = survivalRate), vjust = -.3, hjust = -.3 , size = 5, color = "red") +
   ggtitle("Survival Rate by Fare Class")+
   ylim(0,1)+
-  xlab("Fare Class") + 
+  xlab("Fare Class") +
   ylab("Survival Rate")
 
 SurvivalRateByPclass = full %>%
   filter(Survived != "NA") %>%
-  group_by(Pclass) %>% 
-  summarise(PassengerCount = n(),PassengerSurvived =  sum(Survived == 1), 
+  group_by(Pclass) %>%
+  summarise(PassengerCount = n(),PassengerSurvived =  sum(Survived == 1),
             survivalRate = round(sum(Survived == 1)/n(),3)) %>%
   arrange(desc(Pclass))
+SurvivalRateByPclass
 
-ggplot(SurvivalRateByPclass, aes(x = factor(Pclass), y = survivalRate, group = 1)) + 
+ggplot(SurvivalRateByPclass, aes(x = factor(Pclass), y = survivalRate, group = 1)) +
   geom_line(size = 1.2)+
   geom_text(aes(label = survivalRate), vjust = -.3, hjust = -.3 , size = 5, color = "red") +
   ggtitle("Survival Rate by Passenger Class")+
   ylim(0,1)+
-  xlab("Passenger Class") + 
-  ylab("Survival Rate") 
-
+  xlab("Passenger Class") +
+  ylab("Survival Rate")
 
 SurvivalRateByGenderSib = full %>%
   filter(Survived != "NA") %>%
-  group_by(Sex, SibSp) %>% 
-  summarise(PassengerCount = n(),PassengerSurvived =  sum(Survived == 1), 
-            survivalRate = round(sum(Survived == 1)/n(),3)) 
+  group_by(Sex, SibSp) %>%
+  summarise(PassengerCount = n(),PassengerSurvived =  sum(Survived == 1),
+            survivalRate = round(sum(Survived == 1)/n(),3))
+SurvivalRateByGenderSib
 
 ggplot( SurvivalRateByGenderSib, aes(x = factor(SibSp), y = survivalRate, group = interaction(Sex), color = Sex)) +
   geom_line(size = 1.2) +
   geom_text(aes(label = survivalRate), vjust = -.3, hjust = -.3 , size = 5, color = "red") +
   ggtitle("Survival Rate by Sex - Number of Siblings aboard")+
   ylim(0,1)+
-  xlab("# of sibs") + 
+  xlab("# of sibs") +
   ylab("Survival Rate")
 
-
-SurvivalRateByGenderParch = full %>% 
+SurvivalRateByGenderParch = full %>%
   filter(Survived != "NA") %>%
-  group_by(Sex, Parch) %>% 
-  summarise(PassengerCount = n(),PassengerSurvived =  sum(Survived == 1), 
-            survivalRate = round(sum(Survived == 1)/n(),3)) 
+  group_by(Sex, Parch) %>%
+  summarise(PassengerCount = n(),PassengerSurvived =  sum(Survived == 1),
+            survivalRate = round(sum(Survived == 1)/n(),3))
+SurvivalRateByGenderParch
 
 ggplot( SurvivalRateByGenderParch, aes(x = factor(Parch), y = survivalRate, group = interaction(Sex), color = Sex)) +
   geom_line(size = 1.2) +
   geom_text(aes(label = survivalRate), vjust = -.3, hjust = -.3 , size = 5, color = "red") +
   ggtitle("Survival Rate by Sex - Number of Parent-children aboard")+
   ylim(0,1)+
-  xlab("# of Parent-Children") + 
+  xlab("# of Parent-Children") +
   ylab("Survival Rate")
 
 # Among the people who travels with siblings/parents, are they survived together or not, this method is not perfect due to limitation of
@@ -299,49 +291,130 @@ ggplot( SurvivalRateByGenderParch, aes(x = factor(Parch), y = survivalRate, grou
 full$Surname = str_extract(full$Name, pattern = "[A-z]*" )
 
 SurvivalRateSibSp = full %>%   filter(Survived != "NA" & SibSp != 0) %>%
-  group_by(SibSp, Surname) %>% 
-  summarise(PassengerCount = n(),PassengerSurvived =sum(Survived == 1), 
+  group_by(SibSp, Surname) %>%
+  summarise(PassengerCount = n(),PassengerSurvived =sum(Survived == 1),
             survivalRate = round(sum(Survived == 1)/n(),3)) %>%
-  arrange(desc(survivalRate)) 
+  arrange(desc(survivalRate))
 
 ggplot(SurvivalRateSibSp, aes(x = SibSp, y = survivalRate)) +
   scale_x_continuous( breaks = seq(0,8,1))+
   scale_y_continuous( breaks = seq(0,1,.5))+
-  xlab("# of siblings ") + 
-  ylab("Survival Rate") + 
+  xlab("# of siblings ") +
+  ylab("Survival Rate") +
   ggtitle("Survival Rate per case by number of siblings") +
   geom_jitter(width =.5, color = "blue")
+
 # observation : Most people with siblings/ traveling together chose to either survive together or not survive together
 
-library(MASS)
-?randomForest
-glimpse(full)
-set.seed(100)
+######################################################################
+# PART 2 - IMPUTATION OF MISSING VALUES USING MULTIPLE ALGORITHM (MEDIAN, MISSFORREST, AND MICE)
+######################################################################
+
+
+# Next, we need to work on NAs in Age column, we will use library(missRandom) and library(mice) for the imputation of missing values 
+# in Age column
+
+# To compare before and after missing value imputation, we will do exploratory analysis before and after missing value imputation
+
+# 1st approach we predict the missing Age using median by gender/sex 
+# this approach yield to the best score we can achieve so far 
+
+medians = aggregate(Age ~  Sex, full, median)
+medians
+
+full$Age[is.na(full$Age) & full$Sex == "female"] = 27
+full$Age[is.na(full$Age) & full$Sex == "male"] = 28
+
+# 2nd approach we predict the missing Age using missForrest Package, which predict missing values using random forrest 
+# eliminating columns: PassengerId,Survived,Name,Parch, Ticket,Cabin, because Age should not be correlated to these columns
+#
+# temp = full[-c(1,2,4,8,9,11)]
+# set.seed(100)
+# Age.rf = missForest(temp)
+# full$Age = Age.rf$ximp$Age
+#
+# plotAge.rf1 = ggplot(full[!is.na(full$Age),], aes(x = Age )) +
+#   geom_histogram() + 
+#   facet_grid(~Sex) +
+#   coord_cartesian(ylim =  seq(0,150,5))+
+#   scale_y_continuous(breaks = seq(0,150,5)) +
+#   scale_x_continuous(breaks = seq(0,90,10)) +
+#   ggtitle("Age Dsitribution of Passenger by Gender\n MISSFORREST")
+# 
+# plotAge.rf2 = ggplot(full[!is.na(full$Age),], aes(x = Age, fill = as.factor(Sex))) +
+#   geom_histogram(position = "identity", alpha = .4) +
+#   scale_fill_manual(values=c("green", "purple")) +
+#   scale_y_continuous(breaks = seq(0,150,5))+
+#   scale_x_continuous(breaks = seq(0,90,10)) +
+#   ggtitle("Age Dsitribution of Passenger by Gender\n MISSFORREST") +
+#   labs(fill="")
+# 
+# grid.arrange(plotAgeRaw2, plotAge.rf2 , ncol=2)
+# grid.arrange(plotAgeRaw1, plotAge.rf1 , ncol=2)
+
+# 3rd approach we predict the missing Age using MICE Package, this time we will use "pmm" method, (predictive mean matching)
+# eliminating columns: PassengerId,Survived,Name,Parch, Ticket,Cabin, because Age should not be correlated to these columns
+# temp2 = full[-c(1,2,4,9,11)]
+# set.seed(100)
+# micePmm = mice(temp2 , method = "norm")
+# # micePmm$imp$Age$`3`
+# micePmm$method
+# temp2  = complete(micePmm,3)
+# full$Age = temp2$Age
+# 
+# plotAge.mice1 = ggplot(full[!is.na(full$Age),], aes(x = Age )) +
+#   geom_histogram() +
+#   facet_grid(~Sex) +
+#   coord_cartesian(ylim =  seq(0,100,5))+
+#   scale_y_continuous(breaks = seq(0,100,5)) +
+#   scale_x_continuous(breaks = seq(0,90,10)) +
+#   ggtitle("Age Dsitribution of Passenger by Gender\n MICE")
+# 
+# plotAge.mice2 = ggplot(full[!is.na(full$Age),], aes(x = Age, fill = as.factor(Sex))) +
+#   geom_histogram(position = "identity", alpha = .4) +
+#   scale_fill_manual(values=c("green", "purple")) +
+#   scale_y_continuous(breaks = seq(0,100,5))+
+#   scale_x_continuous(breaks = seq(0,90,10)) +
+#   ggtitle("Age Dsitribution of Passenger by Gender\n MICE") +
+#   labs(fill="")
+# 
+# medians = aggregate(Age ~  Sex, full, median)
+# 
+# plotAge.mice3 = ggplot(full[!is.na(full$Age),], aes(x = Sex, y = Age)) +
+#   geom_boxplot() +
+#   scale_y_continuous(breaks = seq(0,80,5)) +
+#   geom_text( data = medians, aes( label = Age, y = Age), vjust = -.5) +
+#   ggtitle("Age Dsitribution of Passenger by Gender\n MICE")
+# 
+# 
+# grid.arrange(plotAgeRaw1, plotAge.mice1 , ncol=2)
+# grid.arrange(plotAgeRaw2, plotAge.mice2 , ncol=2)
+# grid.arrange(plotAgeRaw3, plotAge.mice3 , ncol=2)
+
 
 full$honorific = as.factor(full$honorific)
 full$GenderAgeClass = as.factor(full$GenderAgeClass)
 
-
 train = full[!is.na(full$Survived),]
 test = full[is.na(full$Survived),]
 
+summary(train)
+summary(test)
+
+set.seed(100)
 titanic.rf = randomForest(factor(Survived) ~ Pclass + Sex + SibSp  + Parch + Fare + Embarked + honorific + GenderAgeClass + FareClass + Age ,
                           data = train)
 
 plot(titanic.rf, ylim=c(0,0.36))
 legend('topright', colnames(titanic.rf$err.rate), col=1:3, fill=1:3)
 
-prediction <- predict(titanic.rf, test)
+prediction = predict(titanic.rf, test)
 submission = data.frame(PassengerId = test$PassengerId, Survived = prediction)
 write.csv(submission, "submission.csv", row.names = FALSE)
 
-table(prediction,genderclassmodel[,2])
-
-mean(prediction != genderclassmodel$Survived)
-
 importance(titanic.rf)
 
-names(full)
+
 
 
 
